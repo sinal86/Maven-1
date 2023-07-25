78178c80638ad772309fc0a98a4c6f7a72c92fc3
Alexey <fier86@mail.ru>
2023-07-25 12:26:02 +0300

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+    }
+}
